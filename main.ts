9e950f5e40b05fa0a8f8bec1d3fd9454c036a530
Mayank Gupta (ARC) <mayank.gupta@iontrading.com>
2019-04-09 12:46:36 +0530

    First Observable using Arrays

--- main.ts
+++ main.ts
@@ -1,1 +1,11 @@
-alert("Hello World");
+import { Observable, from } from 'rxjs';
+
+// Creating Observable from Array
+
+let source = from([1, 2, 3, 4]);
+
+source.subscribe({
+    next: function(value) {
+        console.log(`input data ${value}`);
+    }
+})
